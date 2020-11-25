e94695bd7dea1641a97df0fb91d2ddbab9caf7c3
zhy-git <420998627@qq.com>
2020-11-25 09:12:01 +0800

    邮箱发送

--- m/save.php
+++ m/save.php
@@ -427,10 +427,10 @@ if ($action=="bmtj"){
 				//******************** 配置信息 ********************************
 				$smtpserver = "smtp.163.com";//SMTP服务器
 				$smtpserverport =25;//SMTP服务器端口
-				$smtpusermail = "linyouqun@163.com";//SMTP服务器的用户邮箱
+				$smtpusermail = "zhaihuanyan520@163.com";//SMTP服务器的用户邮箱
 				$smtpemailto = "420998627@qq.com";//发送给谁
-				$smtpuser = "linyouqun@163.com";//SMTP服务器的用户帐号，注：部分邮箱只需@前面的用户名
-				$smtppass = "yanyan38";//SMTP服务器的用户密码
+				$smtpuser = "zhaihuanyan520@163.com";//SMTP服务器的用户帐号，注：部分邮箱只需@前面的用户名
+				$smtppass = "JEYIZEZVGRTIDAUO";//SMTP服务器的用户密码
 				$mailtitle = "您有新的报名信息";//邮件主题
 				$mailcontent = "<h1>{$uname}电话：{$utel}{$lpname}{$ly}</h1>";//邮件内容
 				$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
