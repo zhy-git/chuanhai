20ba5f26992c5af9936a69a4c5f0eb11126c87ab
zhy-git <420998627@qq.com>
2020-12-17 11:36:23 +0800

    百度自动推送代码

--- m/sq2.php
+++ m/sq2.php
@@ -1,1 +1,3 @@
-<link rel="shortcut icon" href="/image/favicon.ico" /><?php echo $config['text2'];?>
+<link rel="shortcut icon" href="/image/favicon.ico" />
+<?php echo $config['text2'];?>
+<?php include_once("baidu_js_push.php") ?> <!-- 百度自动推送代码 -->
--- sq2.php
+++ sq2.php
@@ -1,1 +1,2 @@
 <?php echo $config['text1'];?>
+<?php include_once("baidu_js_push.php") ?> <!-- 百度自动推送代码 -->
