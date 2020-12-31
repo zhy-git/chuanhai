05702f3787c412aa1f31b4f9f7e9c97be85e4dde
zhy-git <420998627@qq.com>
2020-12-31 16:26:37 +0800

    wap抓取手机号码

--- m/save.php
+++ m/save.php
@@ -479,7 +479,8 @@ if ($action=="bmtj"){
 		    //抓取访问者手机号码
 		$isphone = $_GET['isphone'];
         $rows = $mysql->query("select * from `web_zqmobile` where `mobile`='{$isphone}' order by id desc");
-        if (empty($rows)) {
+        $rowsource = $mysql->query("select * from `web_ip` where `mobile`='{$isphone}' and `addtime` <= {$endTime} order by addtime desc");
+        if (empty($rows) || date('Y-m-d',strtotime($rowsource[0]['addtime'])) < date('Y-m-d')) {
         	 $mysql->query("insert into `web_zqmobile`(`mobile`,`addtime`) values ('{$isphone}','{$addtime}')");
 	        	//******************** 配置信息 ********************************
 					$smtpserver = "smtp.163.com";//SMTP服务器
